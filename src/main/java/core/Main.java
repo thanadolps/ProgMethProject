51e5818149218e2ee963bbaca0961d7d21b93726
thanadolps <thanadolps@gmail.com>
2021-11-12 16:43:33 +0700

    Add application(javafx) main class

---
+++ src/main/java/core/Main.java
@@ -0,0 +1,20 @@
+package core;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        var root = new Pane();
+        var scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
